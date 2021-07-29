5ba06f2524b85cc6b08b434d6e754386a7ceadb3
Garry Sotnik <gsotnik@gmail.com>
2021-07-29 17:01:28 -0400

    update sims

--- Model/Simulations_curr_mgmt_alb/curr_mgmt_alb_sim1/BatchFile.bat
+++ Model/Simulations_curr_mgmt_alb/curr_mgmt_alb_sim1/BatchFile.bat
@@ -1,7 +1,7 @@
 rem Batch File to Run a Scenario 
 rem The 'rem' keyword indicates that this is a remark
 
-call landis-ii-7 input_scenario_curr_mgmt_alb.txt
+call landis-ii-7 input_Scenario_curr_mgmt_alb.txt
 
 pause
 
